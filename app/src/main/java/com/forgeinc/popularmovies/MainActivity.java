d3050b1d600d9bbbbcd08c0711f2113424e62248
DavidVallee505 <DavidVallee505@gmail.com>
2016-07-07 23:12:01 -0400

    Creates recyclerView with GridLayoutManager (not displaying image yet)

--- app/src/main/java/com/forgeinc/popularmovies/MainActivity.java
+++ app/src/main/java/com/forgeinc/popularmovies/MainActivity.java
@@ -1,13 +1,33 @@
 package com.forgeinc.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+                                  "http://i.imgur.com/DvpvklR.png"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a grid layout manager
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter
+        mAdapter = new GridAdapter(this, myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
